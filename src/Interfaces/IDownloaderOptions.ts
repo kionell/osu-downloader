a0b4822b0838b2d843f6e23e804a5d6ad1d3dbd3
kionell <sashakostyunin1@gmail.com>
2022-05-11 13:34:27 +0300

    Add new synchronous option for downloader options

--- src/Interfaces/IDownloaderOptions.ts
+++ src/Interfaces/IDownloaderOptions.ts
@@ -11,4 +11,10 @@ export interface IDownloaderOptions {
    * How many files per second will be downloaded. (0 - unlimited).
    */
   filesPerSecond?: number;
+
+  /**
+   * Should file downloading be synchronous or not?
+   * It will overwrite files per second property.
+   */
+  synchronous?: boolean;
 }
