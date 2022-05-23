bc280c395f685f40ad6470090e1b438279f15603
kionell <sashakostyunin1@gmail.com>
2022-05-23 18:40:15 +0300

    Remove failed to read status and add empty file status

--- src/Enums/DownloadStatus.ts
+++ src/Enums/DownloadStatus.ts
@@ -1,8 +1,8 @@
 export enum DownloadStatus {
   FailedToDownload = -3,
   WrongFileFormat = -2,
-  FailedToRead = -1,
-  FailedToWrite = 0,
+  FailedToWrite = -1,
+  EmptyFile = 0,
   FileExists = 1,
   Downloaded = 2,
   Written = 3,
